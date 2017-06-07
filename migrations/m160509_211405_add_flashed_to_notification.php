915773a16ff9540c3b3b4a686cb49a92c1a78252
pistej <pistej2@gmail.com>
2017-06-07 18:51:43 +0200

    correct table quoting and prefixing in migration

--- migrations/m160509_211405_add_flashed_to_notification.php
+++ migrations/m160509_211405_add_flashed_to_notification.php
@@ -6,11 +6,11 @@ class m160509_211405_add_flashed_to_notification extends Migration
 {
     public function up()
     {
-        $this->addColumn('notification', 'flashed', $this->boolean()->notNull());
+        $this->addColumn('{{%notification}}', 'flashed', $this->boolean()->notNull());
     }
 
     public function down()
     {
-        $this->dropColumn('notification', 'flashed');
+        $this->dropColumn('{{%notification}}', 'flashed');
     }
 }
