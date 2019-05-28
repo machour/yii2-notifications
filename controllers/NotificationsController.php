264e94e1a434172adc739d436e050c60b144503b
Breno Coimbra <breno@buskar.me>
2019-05-28 09:37:05 -0300

    adaptado ao sistema

--- controllers/NotificationsController.php
+++ controllers/NotificationsController.php
@@ -8,189 +8,206 @@ use yii\helpers\Url;
 use yii\web\Controller;
 use yii\web\HttpException;
 use yii\web\Response;
+use app\models\Solicitacao;
 
 class NotificationsController extends Controller
 {
-    /**
-     * @var integer The current user id
-     */
-    private $user_id;
+	/**
+	 * @var integer The current user id
+	 */
+	private $user_id;
 
-    /**
-     * @var string The notification class
-     */
-    private $notificationClass;
+	/**
+	 * @var string The notification class
+	 */
+	private $notificationClass;
 
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        Yii::$app->response->format = Response::FORMAT_JSON;
+	/**
+	 * @inheritdoc
+	 */
+	public function init()
+	{
+		Yii::$app->response->format = Response::FORMAT_JSON;
 
-        $this->user_id = $this->module->userId;
-        $this->notificationClass = $this->module->notificationClass;
-        parent::init();
-    }
+		$this->user_id = $this->module->userId;
+		$this->notificationClass = $this->module->notificationClass;
+		parent::init();
+	}
 
-    /**
-     * Poll action
-     *
-     * @param int $seen Whether to show already seen notifications
-     * @return array
-     */
-    public function actionPoll($seen = 0)
-    {
-        $seen = $seen ? 'true' : 'false';
+	/**
+	 * Poll action
+	 *
+	 * @param int $seen Whether to show already seen notifications
+	 * @return array
+	 */
+	public function actionPoll($seen = 0)
+	{
+		$seen = $seen ? 'true' : 'false';
 
-        /** @var Notification $class */
-        $class = $this->notificationClass;
-        $models = $class::find()
-            ->where(['user_id' => $this->user_id])
-            ->andWhere(['or', "seen=$seen", 'flashed=false'])
-            ->orderBy('created_at DESC')
-            ->all();
+		/** @var Notification $class */
+		$class = $this->notificationClass;
+		$models = $class::find()
+			->where('created_at > now() - interval \'1 day\'')
+			->orderBy(['created_at' => SORT_DESC])
+			->all();
 
-        $results = [];
+		$modelsAntigos =  $class::find()
+			->where('created_at < now() - interval \'1 day\'')
+			->orderBy(['created_at' => SORT_DESC])
+			->all();
+		//apaga as notificacoes antigas
+		foreach ($modelsAntigos as $m) {
+			$m->delete();
+		}
 
-        foreach ($models as $model) {
+		$results = [];
 
-            // give user a chance to parse the date as needed
-            $date = \DateTime::createFromFormat($this->module->dbDateFormat, $model->created_at)
-                ->format('Y-m-d H:i:s');
+		foreach ($models as $model) {
 
-            /** @var Notification $model */
-            $results[] = [
-                'id' => $model->id,
-                'type' => $model->type,
-                'title' => $model->getTitle(),
-                'description' => $model->getDescription(),
-                'url' => Url::to(['notifications/rnr', 'id' => $model->id]),
-                'key' => $model->key,
-                'flashed' => $model->flashed,
-                'date' => $date,
-            ];
-        }
-        return $results;
-    }
+			$solicitacao = Solicitacao::findOne($model->key_id);
 
-    /**
-     * Marks a notification as read and redirects the user to the final route
-     *
-     * @param int $id The notification id
-     * @return Response
-     * @throws HttpException Throws an exception if the notification is not
-     *         found, or if it don't belongs to the logged in user
-     */
-    public function actionRnr($id)
-    {
-        $notification = $this->actionRead($id);
-        return $this->redirect(Url::to($notification->getRoute()));
-    }
+			if (!$solicitacao) {
+				$model->delete();
+			}
 
-    /**
-     * Marks a notification as read
-     *
-     * @param int $id The notification id
-     * @return Notification The updated notification record
-     * @throws HttpException Throws an exception if the notification is not
-     *         found, or if it don't belongs to the logged in user
-     */
-    public function actionRead($id)
-    {
-        $notification = $this->getNotification($id);
+			// give user a chance to parse the date as needed
+			$date = \DateTime::createFromFormat($this->module->dbDateFormat, $model->created_at)->format('Y-m-d H:i:s');
 
-        $notification->seen = 1;
-        $notification->save();
+			/** @var Notification $model */
+			$results[] = [
+				'id' => $model->id,
+				'type' => $model->type,
+				'title' => $model->getTitle(),
+				'description' => $model->getDescription(),
+				'url' => Url::to(['notifications/rnr', 'id' => $model->id]),
+				'key' => $model->key,
+				'flashed' => $model->flashed,
+				'date' => $date,
+			];
+		}
+		return $results;
+	}
+	/**
+	 * Marks a notification as read and redirects the user to the final route
+	 *
+	 * @param int $id The notification id
+	 * @return Response
+	 * @throws HttpException Throws an exception if the notification is not
+	 *         found, or if it don't belongs to the logged in user
+	 */
+	public function actionRnr($id)
+	{
+		$notification = $this->actionRead($id);
+		return $this->redirect(Url::to($notification->getRoute()));
+	}
 
-        return $notification;
-    }
+	/**
+	 * Marks a notification as read
+	 *
+	 * @param int $id The notification id
+	 * @return Notification The updated notification record
+	 * @throws HttpException Throws an exception if the notification is not
+	 *         found, or if it don't belongs to the logged in user
+	 */
+	public function actionRead($id)
+	{
+		$notification = $this->getNotification($id);
 
-    /**
-     * Marks all notification as read
-     *
-     * @throws HttpException Throws an exception if the notification is not
-     *         found, or if it don't belongs to the logged in user
-     */
-    public function actionReadAll()
-    {
-        $notificationsIds = Yii::$app->request->post('ids', []);
+		$notification->seen = true;
+		$notification->flashed = true;
+		$notification->type = 'default';
+		$notification->save();
 
-        foreach ($notificationsIds as $id) {
-            $notification = $this->getNotification($id);
+		return $notification;
+	}
 
-            $notification->seen = 1;
-            $notification->save();
-        }
+	/**
+	 * Marks all notification as read
+	 *
+	 * @throws HttpException Throws an exception if the notification is not
+	 *         found, or if it don't belongs to the logged in user
+	 */
+	public function actionReadAll()
+	{
+		$notificationsIds = Yii::$app->request->post('ids', []);
 
-        return true;
-    }
+		foreach ($notificationsIds as $id) {
+			$notification = $this->getNotification($id);
 
-    /**
-     * Delete all notifications
-     *
-     * @throws HttpException Throws an exception if the notification is not
-     *         found, or if it don't belongs to the logged in user
-     */
-    public function actionDeleteAll()
-    {
-        $notificationsIds = Yii::$app->request->post('ids', []);
+			$notification->seen = true;
+			$notification->flashed = true;
+			$notification->type = 'default';
+			$notification->save();
+		}
 
-        foreach ($notificationsIds as $id) {
-            $notification = $this->getNotification($id);
+		return true;
+	}
 
-            $notification->delete();
-        }
+	/**
+	 * Delete all notifications
+	 *
+	 * @throws HttpException Throws an exception if the notification is not
+	 *         found, or if it don't belongs to the logged in user
+	 */
+	public function actionDeleteAll()
+	{
+		$notificationsIds = Yii::$app->request->post('ids', []);
 
-        return true;
-    }
+		foreach ($notificationsIds as $id) {
+			$notification = $this->getNotification($id);
 
-    /**
-     * Deletes a notification
-     *
-     * @param int $id The notification id
-     * @return int|false Returns 1 if the notification was deleted, FALSE otherwise
-     * @throws HttpException Throws an exception if the notification is not
-     *         found, or if it don't belongs to the logged in user
-     */
-    public function actionDelete($id)
-    {
-        $notification = $this->getNotification($id);
-        return $notification->delete();
-    }
+			$notification->delete(false);
+		}
 
-    public function actionFlash($id)
-    {
-        $notification = $this->getNotification($id);
+		return true;
+	}
 
-        $notification->flashed = 1;
-        $notification->save();
+	/**
+	 * Deletes a notification
+	 *
+	 * @param int $id The notification id
+	 * @return int|false Returns 1 if the notification was deleted, FALSE otherwise
+	 * @throws HttpException Throws an exception if the notification is not
+	 *         found, or if it don't belongs to the logged in user
+	 */
+	public function actionDelete($id)
+	{
+		$notification = $this->getNotification($id);
+		return $notification->delete();
+	}
 
-        return $notification;
-    }
+	public function actionFlash($id)
+	{
+		$notification = $this->getNotification($id);
+
+		$notification->flashed = 1;
+		$notification->save();
+
+		return $notification;
+	}
 
 
-    /**
-     * Gets a notification by id
-     *
-     * @param int $id The notification id
-     * @return Notification
-     * @throws HttpException Throws an exception if the notification is not
-     *         found, or if it don't belongs to the logged in user
-     */
-    private function getNotification($id)
-    {
-        /** @var Notification $notification */
-        $class = $this->notificationClass;
-        $notification = $class::findOne($id);
-        if (!$notification) {
-            throw new HttpException(404, "Unknown notification");
-        }
+	/**
+	 * Gets a notification by id
+	 *
+	 * @param int $id The notification id
+	 * @return Notification
+	 * @throws HttpException Throws an exception if the notification is not
+	 *         found, or if it don't belongs to the logged in user
+	 */
+	private function getNotification($id)
+	{
+		/** @var Notification $notification */
+		$class = $this->notificationClass;
+		$notification = $class::findOne($id);
+		if (!$notification) {
+			throw new HttpException(404, "Unknown notification");
+		}
 
-        if ($notification->user_id != $this->user_id) {
-            throw new HttpException(500, "Not your notification");
-        }
+		//        if ($notification->user_id != $this->user_id) {
+		//            throw new HttpException(500, "Not your notification");
+		//        }
 
-        return $notification;
-    }
+		return $notification;
+	}
 }
--- models/Notification.php
+++ models/Notification.php
@@ -21,130 +21,135 @@ use yii\db\ActiveRecord;
 abstract class Notification extends ActiveRecord
 {
 
-    /**
-     * Default notification
-     */
-    const TYPE_DEFAULT = 'default';
-    /**
-     * Error notification
-     */
-    const TYPE_ERROR   = 'error';
-    /**
-     * Warning notification
-     */
-    const TYPE_WARNING = 'warning';
-    /**
-     * Success notification type
-     */
-    const TYPE_SUCCESS = 'success';
+	/**
+	 * Default notification
+	 */
+	const TYPE_DEFAULT = 'default';
+	/**
+	 * Error notification
+	 */
+	const TYPE_ERROR = 'error';
+	/**
+	 * Warning notification
+	 */
+	const TYPE_WARNING = 'warning';
+	/**
+	 * Success notification type
+	 */
+	const TYPE_SUCCESS = 'success';
 
-    /**
-     * @var array List of all enabled notification types
-     */
-    public static $types = [
-        self::TYPE_WARNING,
-        self::TYPE_DEFAULT,
-        self::TYPE_ERROR,
-        self::TYPE_SUCCESS,
-    ];
+	/**
+	 * @var array List of all enabled notification types
+	 */
+	public static $types = [
+		self::TYPE_WARNING,
+		self::TYPE_DEFAULT,
+		self::TYPE_ERROR,
+		self::TYPE_SUCCESS,
+	];
 
-    /**
-     * Gets the notification title
-     *
-     * @return string
-     */
-    abstract public function getTitle();
+	/**
+	 * Gets the notification title
+	 *
+	 * @return string
+	 */
+	abstract public function getTitle();
 
-    /**
-     * Gets the notification description
-     *
-     * @return string
-     */
-    abstract public function getDescription();
+	/**
+	 * Gets the notification description
+	 *
+	 * @return string
+	 */
+	abstract public function getDescription();
 
-    /**
-     * Gets the notification route
-     *
-     * @return string
-     */
-    abstract public function getRoute();
+	/**
+	 * Gets the notification route
+	 *
+	 * @return string
+	 */
+	abstract public function getRoute();
 
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return '{{%notification}}';
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName()
+	{
+		return '{{%notification}}';
+	}
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['type', 'user_id', 'key', 'created_at'], 'required'],
-            [['id', 'key_id', 'created_at'], 'safe'],
-            [['user_id'], 'integer'],
-            [['key_id'], 'string'],
-        ];
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['type', 'user_id', 'key', 'created_at'], 'required'],
+			[['id', 'key_id', 'created_at'], 'safe'],
+			[['user_id'], 'string'],
+			[['key_id'], 'string'],
+		];
+	}
 
-    /**
-     * Creates a notification
-     *
-     * @param string $key
-     * @param integer $user_id The user id that will get the notification
-     * @param string $key_id The foreign instance id
-     * @param string $type
-     * @return bool Returns TRUE on success, FALSE on failure
-     * @throws \Exception
-     */
-    public static function notify($key, $user_id, $key_id = null, $type = self::TYPE_DEFAULT)
-    {
-        $class = self::className();
-        return NotificationsModule::notify(new $class(), $key, $user_id, $key_id, $type);
-    }
+	public static function findNotificacoes()
+	{
+    	return self::findAll(['flashed' => false, 'seen' => false]);
+	}
 
-    /**
-     * Creates a warning notification
-     *
-     * @param string $key
-     * @param integer $user_id The user id that will get the notification
-     * @param string $key_id The notification key id
-     * @return bool Returns TRUE on success, FALSE on failure
-     */
-    public static function warning($key, $user_id, $key_id = null)
-    {
-        return static::notify($key, $user_id, $key_id, self::TYPE_WARNING);
-    }
+	/**
+	 * Creates a notification
+	 *
+	 * @param string $key
+	 * @param integer $user_id The user id that will get the notification
+	 * @param string $key_id The foreign instance id
+	 * @param string $type
+	 * @return bool Returns TRUE on success, FALSE on failure
+	 * @throws \Exception
+	 */
+	public static function notify($key, $user_id, $key_id = null, $type = self::TYPE_DEFAULT)
+	{
+		$class = self::className();
+		return NotificationsModule::notify(new $class(), $key, $user_id, $key_id, $type);
+	}
 
-
-    /**
-     * Creates an error notification
-     *
-     * @param string $key
-     * @param integer $user_id The user id that will get the notification
-     * @param string $key_id The notification key id
-     * @return bool Returns TRUE on success, FALSE on failure
-     */
-    public static function error($key, $user_id, $key_id = null)
-    {
-        return static::notify($key, $user_id, $key_id, self::TYPE_ERROR);
-    }
+	/**
+	 * Creates a warning notification
+	 *
+	 * @param string $key
+	 * @param integer $user_id The user id that will get the notification
+	 * @param string $key_id The notification key id
+	 * @return bool Returns TRUE on success, FALSE on failure
+	 */
+	public static function warning($key, $user_id, $key_id = null)
+	{
+		return static::notify($key, $user_id, $key_id, self::TYPE_WARNING);
+	}
 
 
-    /**
-     * Creates a success notification
-     *
-     * @param string $key
-     * @param integer $user_id The user id that will get the notification
-     * @param string $key_id The notification key id
-     * @return bool Returns TRUE on success, FALSE on failure
-     */
-    public static function success($key, $user_id, $key_id = null)
-    {
-        return static::notify($key, $user_id, $key_id, self::TYPE_SUCCESS);
-    }
+	/**
+	 * Creates an error notification
+	 *
+	 * @param string $key
+	 * @param integer $user_id The user id that will get the notification
+	 * @param string $key_id The notification key id
+	 * @return bool Returns TRUE on success, FALSE on failure
+	 */
+	public static function error($key, $user_id, $key_id = null)
+	{
+		return static::notify($key, $user_id, $key_id, self::TYPE_ERROR);
+	}
+
+
+	/**
+	 * Creates a success notification
+	 *
+	 * @param string $key
+	 * @param integer $user_id The user id that will get the notification
+	 * @param string $key_id The notification key id
+	 * @return bool Returns TRUE on success, FALSE on failure
+	 */
+	public static function success($key, $user_id, $key_id = null)
+	{
+		return static::notify($key, $user_id, $key_id, self::TYPE_SUCCESS);
+	}
 
 }
